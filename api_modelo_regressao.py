647e693505bb88d1c09a3cade4d12e1f3f1a8ad3
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-06-01 18:58:03 -0300

    feat: Delivery modelo como api

---
+++ api_modelo_regressao.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
+import joblib
+
+# Criar uma instância do FastAPI
+app = FastAPI()
+
+# Criar uma classe que terá os dados do request body para a API
+class request_body(BaseModel):
+    horas_estudo: float
+
+# Carregar o modelo de regressão linear para fazer as previsões
+modelo_pontuacao = joblib.load('models/modelo_linear_regression.pkl')
+
+def predict_pontuacao(data: request_body):
+    # Preparar os dados para predição
+    input_feature = [[data.horas_estudo]]
+
+    # Fazer a predição
+    y_pred = modelo_pontuacao.predict(input_feature)[0].astype(float)
+
+    return {'pontuacao': y_pred.tolist()}
